e3bd8dfc70a6723aef38d58fe4921daae293fe49
Jen Jones Arnesen <jennifer@dhis2.org>
2019-03-04 14:14:28 +0000

    33f33dc fixed all tests

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _ItemSelector = require('./ItemSelector');
+
+var _ItemSelector2 = _interopRequireDefault(_ItemSelector);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _ItemSelector2.default;
